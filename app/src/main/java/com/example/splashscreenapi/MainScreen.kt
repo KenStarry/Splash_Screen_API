c6e3a33f743ca935c9cab8d5e2ab7a9398fa8e17
KenStarry <starrycodes@gmail.com>
2023-01-21 14:45:58 +0300

    Added Navigation buttons

---
+++ app/src/main/java/com/example/splashscreenapi/MainScreen.kt
@@ -0,0 +1,51 @@
+package com.example.splashscreenapi
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun MainScreen() {
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Profile screen")
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Settings screen")
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Orders screen")
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
